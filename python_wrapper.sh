ceba1fdff3ce96903a2b819a234568ea78cf0178
Norbert.Neumeister <neumeist@purdue.edu>
2022-09-21 09:42:00 -0400

    updatesfor hammer

--- python_wrapper.sh
+++ python_wrapper.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-source $HOME/miniconda3/etc/profile.d/conda.sh
-conda activate Muon-HAT-2022
+
+#source $HOME/miniconda3/etc/profile.d/conda.sh
+#conda activate Muon-HAT-2022
+
+source /etc/profile.d/modules.sh
+
+module --force purge
+#module spider conda
+module load anaconda/2021.05-py38
+
+conda activate /depot/cms/private/data/Tutorial/Kernels/Muon-HAT-2022
+
 exec python3 "$@"
